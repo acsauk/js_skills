05fc97ce056cc20faf112bf1f1cd94e8f190f6cf
Alex Saunders <acsauk@gmail.com>
2017-07-06 15:54:23 +0100

    creates main.js and includes in the script tag in index.html. Creates and instantiaites TaskCollection with three Task objects in

---
+++ laracasts/es6-intro/src/main.js
@@ -0,0 +1,19 @@
+class TaskCollection {
+  constructor(tasks = []) {
+    this.tasks = tasks;
+  }
+
+  log() {
+    this.tasks.forEach(function(task){
+      console.log(task);
+    });
+  }
+}
+
+class Task {
+  
+}
+
+new TaskCollection([
+  new Task, new Task, new Task
+]).log();
